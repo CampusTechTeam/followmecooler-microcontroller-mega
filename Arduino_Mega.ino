8380f7a78a96b2ed28b2605a10f01aa6258d7b93
Ander Crafter <youtubcrafter@gmail.com>
2020-07-19 16:48:06 +0200

    Add FollowMeCooler Text

--- Arduino_Mega.ino
+++ Arduino_Mega.ino
@@ -24,6 +24,19 @@ void setup()
     
     tft.begin(ID);
     tft.setRotation(1);
+        tft.fillScreen(BLACK);
+    int x = 0; int x2 = 416;
+    int y = 5; int y2 = 5;
+    tft.setAddrWindow(x, y, x + 64 - 1, y + 64 - 1);
+    tft.pushColors((const uint8_t*)ctt_64x64, 64 * 64, 1, false);
+    tft.setAddrWindow(0, 0, tft.width() - 1, tft.height() - 1);
+    tft.setAddrWindow(x2, y2, x2 + 64 - 1, y2 + 64 - 1);
+    tft.pushColors((const uint8_t*)ctt_64x64, 64 * 64, 1, false);
+    tft.setAddrWindow(0, 0, tft.width() - 1, tft.height() - 1);
+    tft.setTextColor(BLUE);
+    tft.setTextSize(4);
+    tft.setCursor(75,5);
+    tft.print("FollowMeCooler");
     //    invertmap(betty_1_bmp + 62, 8960);
     //    while (1);
 }
@@ -32,12 +45,7 @@ void setup()
 
 void loop(void)
 {
+    tft.setCursor(0,0);
 
-    tft.fillScreen(BLACK);
-    int x = 208;
-    int y = 5;
-    tft.setAddrWindow(x, y, x + 64 - 1, y + 64 - 1);
-    tft.pushColors((const uint8_t*)ctt_64x64, 64 * 64, 1, false);
-    tft.setAddrWindow(0, 0, tft.width() - 1, tft.height() - 1);
     delay(5000);
 }
